a5fd673a0d32136f2553ae3dc5c1a2512f11f779
Sampo Haavisto <sampo.haavisto@motleyagency.com>
2016-12-10 10:54:20 +0200

    [Dev] Add variantti

--- fetch-sites.js
+++ fetch-sites.js
@@ -1,13 +1,44 @@
-const casper = require('casper').create();
-const fs = require('fs');
+var casper = require('casper').create();
+var fs = require('fs');
 
-const selectorKanttiini = ".siteorigin-widget-tinymce.textwidget";
+var urls = {
+  "kanttiini": "http://www.smarteat.fi/menu-pitskun-kanttiini/",
+  "variantti": "https://www.taitotalo.com/fi/ravintolat/lounaslistat/variantti-viikon-lounaslista/",
+  // "factory": "http://www.smarteat.fi/menu-pitskun-kanttiini/",
+  "blancco": "http://www.ravintolablancco.com/ravintolat/pitajanmaki/",
+}
 
-casper.start('http://www.smarteat.fi/menu-pitskun-kanttiini/');
+var date = new Date();
+var yyyy = date.getYear();
+var mm = date.getMonth().toString();;
+var dd = date.getDay().toString();
 
-casper.waitForSelector(selectorKanttiini, function() {
-  const html = this.getHTML(selectorKanttiini, true);
-  fs.write("./crawled/kanttiini.html", html, 'w');
+if (mm.length < 1) {
+  mm = "0" +  mm;
+}
+
+if (dd.length < 1) {
+  dd = "0" +  dd;
+}
+
+var tasteParams = yyyy + "/" + mm + "/" + dd;
+urls.taste = "http://www.sodexo.fi/ruokalistat/output/weekly_html/6095/"+tasteParams+"/fi";
+
+const selectors = {
+  kanttiini: ".siteorigin-widget-tinymce.textwidget",
+  variantti: ".lunch-list",
+}
+
+// casper.thenOpen(urls.kanttiini);
+// casper.waitForSelector(selectors.kanttiini, function() {
+//   const html = this.getHTML(selectors.kanttiini, true);
+//   fs.write("./crawled/kanttiini.html", html, 'w');
+// });
+
+casper.start(urls.variantti);
+casper.waitForSelector(selectors.variantti, function() {
+  const html = this.getHTML(selectors.variantti, true);
+  fs.write("./crawled/variantti.html", html, 'w');
 });
 
 casper.run();
