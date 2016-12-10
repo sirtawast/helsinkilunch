cd4b89a384d9e115df11b144c593be76aa22dd37
Sampo Haavisto <sampo.haavisto@motleyagency.com>
2016-12-10 11:16:16 +0200

    [Dev] Fetch taste

--- fetch-sites.js
+++ fetch-sites.js
@@ -2,16 +2,16 @@ var casper = require('casper').create();
 var fs = require('fs');
 
 var urls = {
-  "kanttiini": "http://www.smarteat.fi/menu-pitskun-kanttiini/",
-  "variantti": "https://www.taitotalo.com/fi/ravintolat/lounaslistat/variantti-viikon-lounaslista/",
+  kanttiini: "http://www.smarteat.fi/menu-pitskun-kanttiini/",
+  variantti: "https://www.taitotalo.com/fi/ravintolat/lounaslistat/variantti-viikon-lounaslista/",
+  blancco: "http://www.ravintolablancco.com/ravintolat/pitajanmaki/",
   // "factory": "http://www.smarteat.fi/menu-pitskun-kanttiini/",
-  "blancco": "http://www.ravintolablancco.com/ravintolat/pitajanmaki/",
 }
 
 var date = new Date();
-var yyyy = date.getYear();
-var mm = date.getMonth().toString();;
-var dd = date.getDay().toString();
+var yyyy = new Date().getFullYear();
+var mm = new Date().getMonth().toString();;
+var dd = new Date().getDate().toString();
 
 if (mm.length < 1) {
   mm = "0" +  mm;
@@ -22,23 +22,40 @@ if (dd.length < 1) {
 }
 
 var tasteParams = yyyy + "/" + mm + "/" + dd;
+
 urls.taste = "http://www.sodexo.fi/ruokalistat/output/weekly_html/6095/"+tasteParams+"/fi";
 
 const selectors = {
+  blancco: "",
   kanttiini: ".siteorigin-widget-tinymce.textwidget",
   variantti: ".lunch-list",
+  taste: "h1",
 }
 
-casper.thenOpen(urls.kanttiini);
-casper.waitForSelector(selectors.kanttiini, function() {
-  const html = this.getHTML(selectors.kanttiini, true);
-  fs.write("./crawled/kanttiini.html", html, 'w');
-});
+// TODO: Make a map and loop these to keep DRY
+casper.start(urls.kanttiini);
+// casper.waitForSelector(selectors.kanttiini, function() {
+//   const html = this.getHTML(selectors.kanttiini, true);
+//   fs.write("./crawled/kanttiini.html", html, 'w');
+// });
 
-casper.start(urls.variantti);
-casper.waitForSelector(selectors.variantti, function() {
-  const html = this.getHTML(selectors.variantti, true);
-  fs.write("./crawled/variantti.html", html, 'w');
+// casper.thenOpen(urls.variantti);
+// casper.waitForSelector(selectors.variantti, function() {
+//   const html = this.getHTML(selectors.variantti, true);
+//   fs.write("./crawled/variantti.html", html, 'w');
+// });
+
+// casper.thenOpen(urls.blancco);
+// casper.waitForSelector(selectors.blancco, function() {
+//   const html = this.getHTML(selectors.blancco, true);
+//   fs.write("./crawled/blancco.html", html, 'w');
+// });
+
+casper.thenOpen(urls.taste);
+casper.waitForSelector(selectors.taste, function() {
+  var elements = this.getElementsInfo('table');
+  var element = elements[1];
+  fs.write("./crawled/taste.html", element.html, 'w');
 });
 
 casper.run();
