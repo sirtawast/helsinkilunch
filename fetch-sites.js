e5dfaec036d3574ff6c88fec94fd122f983d5a55
Sampo Haavisto <sampo.haavisto@motleyagency.com>
2016-12-10 10:34:51 +0200

    [Dev] Start crawlin'

---
+++ fetch-sites.js
@@ -0,0 +1,13 @@
+const casper = require('casper').create();
+const fs = require('fs');
+
+const selectorKanttiini = ".siteorigin-widget-tinymce.textwidget";
+
+casper.start('http://www.smarteat.fi/menu-pitskun-kanttiini/');
+
+casper.waitForSelector(selectorKanttiini, function() {
+  const html = this.getHTML(selectorKanttiini, true);
+  fs.write("kanttiini.html", html, 'w');
+});
+
+casper.run();
