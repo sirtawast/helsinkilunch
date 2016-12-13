9a6c92ada82f3363d315db9df7b57a6685e55f4d
Sampo Haavisto <sampo.haavisto@gmail.com>
2016-12-13 20:14:45 +0200

    [Dev] Restaurants and styles

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,14 @@
 $(document).ready(() => {
 
-  const restaurants = ['taste', 'variantti', 'kanttiini']
+  const restaurants = ['taste', 'variantti', 'kanttiini', 'blancco', 'factory']
+
+  const days = [
+  	{name: "Maanantai"},
+  	{name: "Tiistai"},
+  	{name: "Keskiviikko"},
+  	{name: "Torstai"},
+  	{name: "Perjantai"},
+  ]
 
   restaurants.forEach((o) => {
     $.ajax({
@@ -8,7 +16,15 @@ $(document).ready(() => {
       type: 'get',
     }).done((res) => {
       $(`#${o}`).html(res);
+
+      http://lorempixel.com/400/200/food/10/
+
+      days.forEach((o) => {
+  		  var re = new RegExp(o.name, "gi");
+	  	  $('.tab-content').html($('.tab-content').html().replace(re, `<div class="day">${o.name}</div>`));
+      });
     });
   })
+
 
 });
