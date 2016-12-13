ae80c26b8cecb6e771dbd24f1b8cfae387e3f119
Sampo Haavisto <sampo.haavisto@gmail.com>
2016-12-13 20:14:24 +0200

    [Dev] Moar restaurants, 'sanitation' of a kind for tags

--- fetch-sites.js
+++ fetch-sites.js
@@ -4,8 +4,8 @@ var fs = require('fs');
 var urls = {
   kanttiini: "http://www.smarteat.fi/menu-pitskun-kanttiini/",
   variantti: "https://www.taitotalo.com/fi/ravintolat/lounaslistat/variantti-viikon-lounaslista/",
-  blancco: "http://www.ravintolablancco.com/ravintolat/pitajanmaki/",
-  // "factory": "http://www.smarteat.fi/menu-pitskun-kanttiini/",
+  blancco: "http://www.ravintolablancco.com/lounaviikko/pitajanmaki/",
+  factory: "http://www.ravintolafactory.com/lounasravintolat/ravintolat/helsinki-pitajanmaki/",
 }
 
 var date = new Date();
@@ -26,35 +26,78 @@ var tasteParams = yyyy + "/" + mm + "/" + dd;
 urls.taste = "http://www.sodexo.fi/ruokalistat/output/weekly_html/6095/"+tasteParams+"/fi";
 
 const selectors = {
-  blancco: "",
+  blancco: ".entry-content",
   kanttiini: ".siteorigin-widget-tinymce.textwidget",
   variantti: ".lunch-list",
   taste: "h1",
+  factory: '.post_content',
 }
 
 // TODO: Make a map and loop these to keep DRY
 casper.start(urls.kanttiini);
 casper.waitForSelector(selectors.kanttiini, function() {
   const html = this.getHTML(selectors.kanttiini, true);
-  fs.write("./static/crawled/kanttiini.html", html, 'w');
+  fs.write("./static/crawled/kanttiini.html", replaceAllTags(html), 'w');
+  this.echo('Kanttiini OK');
 });
 
 casper.thenOpen(urls.variantti);
 casper.waitForSelector(selectors.variantti, function() {
   const html = this.getHTML(selectors.variantti, true);
-  fs.write("./static/crawled/variantti.html", html, 'w');
+  fs.write("./static/crawled/variantti.html", replaceAllTags(html), 'w');
+  this.echo('Variantti OK');
 });
 
-// casper.thenOpen(urls.blancco);
-// casper.waitForSelector(selectors.blancco, function() {
-//   const html = this.getHTML(selectors.blancco, true);
-//   fs.write("./static/crawled/blancco.html", html, 'w');
-// });
+casper.thenOpen(urls.blancco);
+casper.waitForSelector(selectors.blancco, function() {
+  const html = this.getHTML(selectors.blancco, true);
+  fs.write("./static/crawled/blancco.html", replaceAllTags(html), 'w');
+  this.echo('Blancco OK');
+});
+
+casper.thenOpen(urls.factory);
+casper.waitForSelector(selectors.factory, function() {
+  const html = this.getHTML(selectors.factory, true);
+  fs.write("./static/crawled/factory.html", replaceAllTags(html), 'w');
+  this.echo('Factory OK');
+});
 
 casper.thenOpen(urls.taste);
 casper.waitForSelector(selectors.taste, function() {
   var elements = this.getElementsInfo('table');
-  fs.write("./static/crawled/taste.html", elements[0].html + elements[1].html, 'w');
+  fs.write("./static/crawled/taste.html", replaceAllTags(elements[0].html) + replaceAllTags(elements[1].html), 'w');
+  this.echo('Taste OK');
 });
 
-casper.run();
+casper.run(function(){
+  this.echo('Crawling done!');
+});
+
+var replaceAllTags = function(html) {
+  var divOpen = "<div";
+  var divClose = "<div>";
+
+  var spanOpen = "<span";
+  var spanClose = "</span>";
+
+  return html = html
+    .replace(/<h1>/gi, '').replace(/<\/h1>/gi, '')
+    .replace(/<h2/gi, divOpen).replace(/<\/h2>/gi, divClose)
+    .replace(/<h3/gi, divOpen).replace(/<\/h3>/gi, divClose)
+    .replace(/<h4/gi, divOpen).replace(/<\/h4>/gi, divClose)
+    .replace(/<h5/gi, divOpen).replace(/<\/h5>/gi, divClose)
+    .replace(/<h6/gi, divOpen).replace(/<\/h6>/gi, divClose)
+    .replace(/<tr/gi, divOpen).replace(/<\/tr>/gi, divClose)
+    .replace(/<td/gi, divOpen).replace(/<\/td>/gi, divClose)
+    .replace(/<th/gi, divOpen).replace(/<\/th>/gi, divClose)
+    .replace(/<p/gi, divOpen).replace(/<\/p>/gi, divClose)
+    .replace(/<ul/gi, divOpen).replace(/<\/ul>/gi, divClose)
+    .replace(/<li/gi, divOpen).replace(/<\/li>/gi, divClose)
+    .replace(/<table/gi, divOpen).replace(/<\/table>/gi, divClose)
+    .replace(/<tbody/gi, divOpen).replace(/<\/tbody>/gi, divClose)
+    .replace(/<tfoot/gi, divOpen).replace(/<\/tfoot>/gi, divClose)
+    .replace(/<b/gi, spanOpen).replace(/<\/b>/gi, spanClose)
+    .replace(/<i/gi, spanOpen).replace(/<\/i>/gi, spanClose)
+    .replace(/<strong/gi, spanOpen).replace(/<\/strong>/gi, spanClose)
+    .replace(/<em/gi, spanOpen).replace(/<\/em>/gi, spanClose)
+}
