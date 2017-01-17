4889d2e1edc1c56141aa6ae7e2cfd2c54c4e2e2c
Sampo Haavisto <sampo.haavisto@motleyagency.com>
2017-01-17 09:07:53 +0200

    [Dev] Code styling

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const serveStatic = require('serve-static')
-const compression = require('compression')
+const express = require('express');
+const serveStatic = require('serve-static');
+const compression = require('compression');
 
-const app = express()
+const app = express();
 
 app.use(compression());
 
-app.use(serveStatic('static/', {'index': ['index.html']}))
-app.listen(3000)
+app.use(serveStatic('static/', {'index': ['index.html']}));
+app.listen(3000);
