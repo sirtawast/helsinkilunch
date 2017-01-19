0453ca294a9b5668daedca1a4bff578fb563b495
Sampo Haavisto <sampo.haavisto@motleyagency.com>
2017-01-19 22:49:51 +0200

    [Dev] Var name again

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -14,10 +14,11 @@ REPOSITORY=`git config remote.origin.url`
 GITHUB_LINK=${REPOSITORY/https:\/\/github.com\//git@github.com:}
 COMMIT_SHA=`git rev-parse --verify HEAD`
 
+git stash
 git checkout gh-pages
 git stash
 mv /tmp/crawled/ crawled/
 
 git add .
-git commit -m "[Travis] Update crawled files (${SHA})"
+git commit -m "[Travis] Update crawled files (${COMMIT_SHA})"
 git push $GITHUB_LINK gh-pages
