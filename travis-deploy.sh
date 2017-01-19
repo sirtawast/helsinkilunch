656332b00a523e81ef410da044758a42dd52118f
Sampo Haavisto <sampo.haavisto@motleyagency.com>
2017-01-19 22:28:32 +0200

    [Dev] Travis gh-pages additions

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -1,14 +1,25 @@
 #!/bin/sh
+set -e
 
 if [[ "$TRAVIS_BRANCH" != master ]]; then
   echo "Not building on 'master' so not deploying"
   exit 0
 fi
 
-git checkout gh-pages;
-git stash;
-mv /tmp/crawled/ crawled/;
+eval `ssh-agent -s`
+ssh-add /tmp/deploy_key
 
-git add .;
-git commit -m "[Travis] Update crawled files";
-git push;
+git config user.name "Travis CI"
+git config user.email "spook@example.com"
+
+REPOSITORY=`git config remote.origin.url`
+GITHUB_LINK=${REPO/https:\/\/github.com\//git@github.com:}
+COMMIT_SHA=`git rev-parse --verify HEAD`
+
+git checkout gh-pages
+git stash
+mv /tmp/crawled/ crawled/
+
+git add .
+git commit -m "[Travis] Update crawled files"
+git push $GITHUB_LINK gh-pages
