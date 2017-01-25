29beaccdace2316e6599a560a5621ea78e010ceb
sampohaavisto <sampo.haavisto@motleyagency.com>
2017-01-25 08:25:56 +0200

    [skip ci] ... and also remember to remove the longer version of the param

--- deploy.sh
+++ deploy.sh
@@ -19,8 +19,8 @@ git reset --hard origin/master
 git fetch --all
 git checkout -b gh-pages --track origin/gh-pages
 
-rsync -auvP --update /tmp/crawled/* crawled/ 
-rsync -auvP --update /tmp/static/* ./
+rsync -auvP /tmp/crawled/* crawled/ 
+rsync -auvP /tmp/static/* ./
 
 git add .
 git commit -m "[Travis] Update crawled files (${COMMIT_SHA})"
