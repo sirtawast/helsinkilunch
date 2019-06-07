b5eeea0fc96ee7b82b9ffc25ea0dc540b08380c1
sirtawast <sampo.tawast@gmail.com>
2019-06-07 09:04:49 +0300

    Update deps

--- src/App.vue
+++ src/App.vue
@@ -18,9 +18,6 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  margin-top: 40px;
-  margin-left: 40px;
-  right-left: 40px;
-
+  margin: 40px;
 }
 </style>
